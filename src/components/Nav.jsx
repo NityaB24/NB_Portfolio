f14c7e03417c44ead2991b3d202297880f52df6a
NityaB24 <nityabalar@gmail.com>
2024-06-05 12:51:19 +0530

    menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './nav_styles.css'
+import './nav_styles.css';
+
 function Nav() {
   const [isHidden, setIsHidden] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,7 +21,17 @@ function Nav() {
     };
   }, [prevScrollPos]);
 
-  
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isMenuOpen]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div id='nav' className={`fixed z-[999] w-full px-20 py-8 font-['Neue Montreal'] flex justify-between items-center ${isHidden ? 'hidden' : 'show'}`}>
@@ -31,14 +42,18 @@ function Nav() {
       </div>
       <div className="links flex gap-10">
         <a href="#about" className="about text-md capitalize font-regular">About</a>
-        <a href="#featured" className="work text-md capitalize font-regular" >Work</a>
+        <a href="#featured" className="work text-md capitalize font-regular">Work</a>
         <a href="#skills" className="skills text-md capitalize font-regular">Skills</a>
         <a href="#contact" className="contact text-md capitalize font-regular">Contact</a>
-        <a href="#menu" class="menu text-md capitalize font-regular ">Menu</a>
-        <div className="side">
-          
-        </div>
-
+        <a href="#menu" className="menu text-md capitalize font-regular" onClick={toggleMenu}>Menu</a>
+        {isMenuOpen && (
+          <div className="mobile-menu flex flex-col gap-4">
+            <a href="#about" className="text-md capitalize font-regular" onClick={toggleMenu}>About</a>
+            <a href="#featured" className="text-md capitalize font-regular" onClick={toggleMenu}>Work</a>
+            <a href="#skills" className="text-md capitalize font-regular" onClick={toggleMenu}>Skills</a>
+            <a href="#contact" className="text-md capitalize font-regular" onClick={toggleMenu}>Contact</a>
+          </div>
+        )}
       </div>
     </div>
   );
